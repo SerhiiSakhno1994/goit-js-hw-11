cd447b4a3e3a5e51a55b3865c3d2e2f455182c56
Serhii <89796587+SerhiiSakhno1994@users.noreply.github.com>
2022-02-22 00:55:28 +0200

    render

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
 import './css/styles.css';
+import ApiPixabay from './js/pixabayAPI';
+import hitsTpl from './hits.hbs';
+import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  searchForm: document.querySelector('.js-search-form'),
+    articlesContainer: document.querySelector('.js-articles-container'),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+};
+
+const newApiPixabay = new ApiPixabay();
+
+    
+
+
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+
+
+
+function onSearch(e) {
+    e.preventDefault();
+
+    
+    newApiPixabay.query = e.currentTarget.elements.query.value;
+    newApiPixabay.resetPage();
+    newApiPixabay.fetchArticles().then(({ hits, totalHits }) => {
+
+        if (newApiPixabay.query === '') {
+      Notiflix.Notify.failure('Oops... Please enter the text');
+    } else if (hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.',
+      );
+      return;
+    }
+
+    Notiflix.Notify.success(`"Hooray! We found ${totalHits} images."`);
+        clearHitsContainer();
+        appenHitsMarkup(hits)
+
+    });
+
+
+}
+
+
+
+function onLoadMore() { 
+  newApiPixabay.fetchArticles().then(appenHitsMarkup);
+
+};
+
+function appenHitsMarkup(hits) {
+  refs.articlesContainer.insertAdjacentHTML('beforeend', hitsTpl(hits));
+}
+
+function clearHitsContainer() {
+  refs.articlesContainer.innerHTML = '';
+}
